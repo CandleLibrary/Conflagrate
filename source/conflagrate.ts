ea3f686fbd9832d4134a7d600e1820a899ca749b
galactrax <anthonycweathersby@gmail.com>
2020-03-01 18:26:37 -0700

    Add source files that have been transplanted removed from cfw.wick

---
+++ source/conflagrate.ts
@@ -0,0 +1,9 @@
+import { filter } from "./filter.js";
+import { make_replaceable } from "./replaceable.js";
+import { traverse } from "./traverse.js";
+
+export {
+    filter,
+    make_replaceable,
+    traverse
+};
