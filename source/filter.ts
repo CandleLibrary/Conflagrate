b73609296c16452bb32f33147f68005396267fc0
galactrax <anthonycweathersby@gmail.com>
2020-03-01 18:39:27 -0700

    Add description to filter function

--- source/filter.ts
+++ source/filter.ts
@@ -5,7 +5,12 @@ export class filterYielder<T, K extends keyof T> extends Yielder<T, K> {
     types: Set<string>;
 
 }
-
+/**
+ * Filters nodes and yields only this whose property {key} matches one of the values {types}
+ * 
+ * @param key - A property name on the node that should be tested for a match.
+ * @param types  - A list of possible values that we want property {key} to be.
+ */
 export function filter<T, K extends keyof T, D extends keyof T>(key: D, ...types: string[]): Yielder<T, K> {
 
     const obj = Object.assign(new filterYielder<T, K>(), {
