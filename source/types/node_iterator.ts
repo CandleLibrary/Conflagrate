aa0d6414d3d7c9f8a83c0175eb4373fec165e027
Anthony <anthonycweathersby@gmail.com>
2020-03-05 13:55:53 -0700

    Move ASTIterator definition to new file

---
+++ source/types/node_iterator.ts
@@ -0,0 +1,10 @@
+import { Yielder } from "../yielder.js";
+
+export type ASTIterator<T, K extends keyof T> = Iterable<T> & {
+    /**
+     * Adds a Yielder to the end of the yield chain.
+     *
+     * @param next_yielder A Node Yielder
+     */
+    then: (arg0: Yielder<T, K>) => ASTIterator<T, K>;
+};
