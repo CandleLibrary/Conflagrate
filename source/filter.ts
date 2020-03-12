0693c505ff35117fd36e0fd1030b289fac07188d
galactrax <anthonycweathersby@gmail.com>
2020-03-11 20:49:42 -0600

    Modify typing on filter yielder

--- source/filter.ts
+++ source/filter.ts
@@ -2,16 +2,16 @@ import { Yielder } from "./yielder.js";
 
 export class filterYielder<T, K extends keyof T> extends Yielder<T, K> {
     filter_key: string;
-    types: Set<string>;
+    types: Set<any>;
 
 }
 /**
- * Filters nodes and yields only those whose property {key} matches one of the values {types}
+ * Filters nodes and yields only those whose property `key` matches one of the values in `types`
  * 
  * @param key - A property name on the node that should be tested for a match.
- * @param types  - A list of possible values that we want property {key} to be.
+ * @param types  - A list of possible values that we want property `key` to be.
  */
-export function filter<T, K extends keyof T, D extends keyof T>(key: D, ...types: string[]): Yielder<T, K> {
+export function filter<T, K extends keyof T, D extends keyof T>(key: D, ...types: any[]): Yielder<T, K> {
 
     const obj = Object.assign(new filterYielder<T, K>(), {
         filter_key: key,
@@ -25,7 +25,7 @@ export function filter<T, K extends keyof T, D extends keyof T>(key: D, ...types
 
 function filterYield<T>(node: T, stack_pointer: number, node_stack: T[], val_length_stack: number[]): T | null {
 
-    const type = node[this.filter_key] + "";
+    const type = node[this.filter_key];
 
     if (this.types.has(type))
         return this.yieldNext(node, stack_pointer, node_stack, val_length_stack);
