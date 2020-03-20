590f915fe9701c329c5db66ef8a0cee10072b20f
galactrax <anthonycweathersby@gmail.com>
2020-03-19 22:13:18 -0600

    Instrument cfw.test

---
+++ test/candlefw.conflagrate.spec.js
@@ -0,0 +1,66 @@
+import { createSourceMapJSON, decodeJSONSourceMap, getSourceLineColumn, createSourceMap, createSourceMapEntry, getPositionLexerFromJSONSourceMap, skip_root, traverse, double_back_traverse, extract, filter, make_replaceable, make_skippable, replace, add_parent, bit_filter } from "../build/library/conflagrate.js";
+"@candlefw/conflagrate test spec";
+
+"TODO - test: createSourceMapJSON";
+
+((createSourceMapJSON));
+
+"TODO - test: decodeJSONSourceMap";
+
+((decodeJSONSourceMap));
+
+"TODO - test: getSourceLineColumn";
+
+((getSourceLineColumn));
+
+"TODO - test: createSourceMap";
+
+((createSourceMap));
+
+"TODO - test: createSourceMapEntry";
+
+((createSourceMapEntry));
+
+"TODO - test: getPositionLexerFromJSONSourceMap";
+
+((getPositionLexerFromJSONSourceMap));
+
+"TODO - test: skip_root";
+
+((skip_root));
+
+"TODO - test: traverse";
+
+((traverse));
+
+"TODO - test: double_back_traverse";
+
+((double_back_traverse));
+
+"TODO - test: extract";
+
+((extract));
+
+"TODO - test: filter";
+
+((filter));
+
+"TODO - test: make_replaceable";
+
+((make_replaceable));
+
+"TODO - test: make_skippable";
+
+((make_skippable));
+
+"TODO - test: replace";
+
+((replace));
+
+"TODO - test: add_parent";
+
+((add_parent));
+
+"TODO - test: bit_filter";
+
+((bit_filter));
