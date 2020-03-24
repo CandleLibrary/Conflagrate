976e6facd7bae32de6970933d6b588bedceb63df
galactrax <anthonycweathersby@gmail.com>
2020-03-24 07:46:18 -0600

    Add test file

--- test/candlefw.conflagrate.spec.js
+++ test/candlefw.conflagrate.spec.js
@@ -1,66 +1,199 @@
-import { createSourceMapJSON, decodeJSONSourceMap, getSourceLineColumn, createSourceMap, createSourceMapEntry, getPositionLexerFromJSONSourceMap, skip_root, traverse, double_back_traverse, extract, filter, make_replaceable, make_skippable, replace, add_parent, bit_filter } from "../build/library/conflagrate.js";
+
+
+import {
+    createSourceMapJSON,
+    decodeJSONSourceMap,
+    getSourceLineColumn,
+    createSourceMap,
+    createSourceMapEntry,
+    getPositionLexerFromJSONSourceMap,
+    skip_root,
+    double_back_traverse,
+    extract,
+    filter,
+    make_replaceable,
+    make_skippable,
+    replace,
+    add_parent,
+    bit_filter,
+    err
+} from "../build/library/conflagrate.js";
+
+const r = Math.random, rn = Math.round;
+
+function createTestTree(max_children = 3, node_count = { num: 0 }, types = [], bit_types = [], max_depth = 3) {
+
+    const
+        children_count = max_depth > 0 ? Math.max(rn(r() * max_children - 1), 0) : 0,
+        types_index = Math.max(rn((r() * types.length) - 1), 0),
+        bit_types_index = Math.max(rn((r() * bit_types.length) - 1), 0);
+
+    node_count.num++;
+
+    return {
+
+        bit_type: bit_types[bit_types_index],
+
+        type: types[types_index],
+
+        children: Array(children_count).fill(
+            null
+        ).map(() => createTestTree(
+            max_children,
+            node_count,
+            types,
+            bit_types,
+            max_depth - 1
+        ))
+    };
+}
+
 "@candlefw/conflagrate test spec";
+{
+    const random = (Math.random() * 100) | 0;
 
-"TODO - test: createSourceMapJSON";
 
-((createSourceMapJSON));
+    "Test traverse"; "#";
+    let a = 0;
+    const b = { num: 0 };
+    const r = createTestTree(5, b, ["A", "B", "C"], [5, 2, 4, 8], 8);
 
-"TODO - test: decodeJSONSourceMap";
+    ((err() == 2));
 
-((decodeJSONSourceMap));
+}
 
-"TODO - test: getSourceLineColumn";
+SKIP: {
+    "TODO: Test createSourceMapJSON";
 
-((getSourceLineColumn));
+    "#";
 
-"TODO - test: createSourceMap";
+    ((createSourceMapJSON));
+}
 
-((createSourceMap));
+SKIP: {
 
-"TODO - test: createSourceMapEntry";
+    "TODO: Test decodeJSONSourceMap";
 
-((createSourceMapEntry));
+    "#";
 
-"TODO - test: getPositionLexerFromJSONSourceMap";
+    ((decodeJSONSourceMap));
+}
 
-((getPositionLexerFromJSONSourceMap));
+SKIP: {
 
-"TODO - test: skip_root";
+    "TODO: Test getSourceLineColumn";
 
-((skip_root));
+    "#";
 
-"TODO - test: traverse";
+    ((getSourceLineColumn));
+}
 
-((traverse));
+SKIP: {
 
-"TODO - test: double_back_traverse";
+    "TODO: Test createSourceMap";
 
-((double_back_traverse));
+    "#";
 
-"TODO - test: extract";
+    ((createSourceMap));
+}
 
-((extract));
+SKIP: {
 
-"TODO - test: filter";
+    "TODO: Test createSourceMapEntry";
 
-((filter));
+    "#";
 
-"TODO - test: make_replaceable";
+    ((createSourceMapEntry));
+}
 
-((make_replaceable));
+SKIP: {
 
-"TODO - test: make_skippable";
+    "TODO: Test getPositionLexerFromJSONSourceMap";
 
-((make_skippable));
+    "#";
 
-"TODO - test: replace";
+    ((getPositionLexerFromJSONSourceMap));
+}
 
-((replace));
+SKIP: {
 
-"TODO - test: add_parent";
+    "TODO: Test skip_root";
 
-((add_parent));
+    "#";
 
-"TODO - test: bit_filter";
+    ((skip_root));
+}
 
-((bit_filter));
+
+
+SKIP: {
+
+    "TODO: Test double_back_traverse";
+
+    "#";
+
+    ((double_back_traverse));
+}
+
+SKIP: {
+
+    "TODO: Test extract";
+
+    "#";
+
+    ((extract));
+}
+
+SKIP: {
+
+    "TODO: Test filter";
+
+    "#";
+
+    ((filter));
+}
+
+SKIP: {
+
+    "TODO: Test make_replaceable";
+
+    "#";
+
+    ((make_replaceable));
+}
+
+SKIP: {
+
+    "TODO: Test make_skippable";
+
+    "#";
+
+    ((make_skippable));
+}
+
+SKIP: {
+
+    "TODO: Test replace";
+
+    "#";
+
+    ((replace));
+}
+
+SKIP: {
+
+    "TODO: Test add_parent";
+
+    "#";
+
+    ((add_parent));
+}
+
+SKIP: {
+
+    "TODO: Test bit_filter";
+
+    "#";
+
+    ((bit_filter));
+}
