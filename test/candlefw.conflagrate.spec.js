16682e9e6f417330d53f6b19dd2df192d52cdf3f
galactrax <anthonycweathersby@gmail.com>
2020-03-26 17:23:56 -0600

    Add test spec

--- test/candlefw.conflagrate.spec.js
+++ test/candlefw.conflagrate.spec.js
@@ -16,7 +16,7 @@ import {
     replace,
     add_parent,
     bit_filter,
-    err
+    traverse,
 } from "../build/library/conflagrate.js";
 
 const r = Math.random, rn = Math.round;
@@ -24,7 +24,7 @@ const r = Math.random, rn = Math.round;
 function createTestTree(max_children = 3, node_count = { num: 0 }, types = [], bit_types = [], max_depth = 3) {
 
     const
-        children_count = max_depth > 0 ? Math.max(rn(r() * max_children - 1), 0) : 0,
+        children_count = max_depth > 0 ? Math.max(rn(r() * max_children - 1), 1) : 0,
         types_index = Math.max(rn((r() * types.length) - 1), 0),
         bit_types_index = Math.max(rn((r() * bit_types.length) - 1), 0);
 
@@ -53,147 +53,21 @@ function createTestTree(max_children = 3, node_count = { num: 0 }, types = [], b
     const random = (Math.random() * 100) | 0;
 
 
-    "Test traverse"; "#";
+    "Test Traversal"; "#";
     let a = 0;
+
     const b = { num: 0 };
-    const r = createTestTree(5, b, ["A", "B", "C"], [5, 2, 4, 8], 8);
 
-    ((err() == 2));
+    const r = createTestTree(5, b, ["A", "B", "C"], [5, 2, 4, 8], 20);
 
-}
+    SEQUENCE: {
+        for (const { node } of traverse(r, "children")) { a++; }
+        "All Nodes Traversed";
+        ((a === b.num));
+    }
 
-SKIP: {
-    "TODO: Test createSourceMapJSON";
-
-    "#";
-
-    ((createSourceMapJSON));
-}
-
-SKIP: {
-
-    "TODO: Test decodeJSONSourceMap";
-
-    "#";
-
-    ((decodeJSONSourceMap));
-}
-
-SKIP: {
-
-    "TODO: Test getSourceLineColumn";
-
-    "#";
-
-    ((getSourceLineColumn));
-}
-
-SKIP: {
-
-    "TODO: Test createSourceMap";
-
-    "#";
-
-    ((createSourceMap));
-}
-
-SKIP: {
-
-    "TODO: Test createSourceMapEntry";
-
-    "#";
-
-    ((createSourceMapEntry));
-}
-
-SKIP: {
-
-    "TODO: Test getPositionLexerFromJSONSourceMap";
-
-    "#";
-
-    ((getPositionLexerFromJSONSourceMap));
-}
-
-SKIP: {
-
-    "TODO: Test skip_root";
-
-    "#";
-
-    ((skip_root));
-}
-
-
-
-SKIP: {
-
-    "TODO: Test double_back_traverse";
-
-    "#";
-
-    ((double_back_traverse));
-}
-
-SKIP: {
-
-    "TODO: Test extract";
-
-    "#";
-
-    ((extract));
-}
-
-SKIP: {
-
-    "TODO: Test filter";
-
-    "#";
-
-    ((filter));
-}
-
-SKIP: {
-
-    "TODO: Test make_replaceable";
-
-    "#";
-
-    ((make_replaceable));
-}
-
-SKIP: {
-
-    "TODO: Test make_skippable";
-
-    "#";
-
-    ((make_skippable));
-}
-
-SKIP: {
-
-    "TODO: Test replace";
-
-    "#";
-
-    ((replace));
-}
-
-SKIP: {
-
-    "TODO: Test add_parent";
-
-    "#";
-
-    ((add_parent));
-}
-
-SKIP: {
-
-    "TODO: Test bit_filter";
-
-    "#";
-
-    ((bit_filter));
+    for (const { node } of traverse(r, "children").filter("type", "B", "C")) {
+        "Filtering";
+        ((node.type !== "A"));
+    }
 }
