5eb10df2f2497455e4d92205716d04b6fc85eed6
galactrax <anthonycweathersby@gmail.com>
2020-07-24 12:27:47 -0600

    Formate

--- source/render/render.ts
+++ source/render/render.ts
@@ -131,39 +131,44 @@ function getRenderRule<T>(node: T & { pos: Lexer, nodes: T[], type: number; }, f
     };
 }
 
+/**
+    A template pattern may contain template insertion points marked by a [$]
+    character. For each insertion point there is an action to perform depending
+    on the surrounding syntax:
+    
+    1. Value-Index-Insertion
+
+    \@\\n+\?? => This indicates the rendering of the node contained in parent.nodes[<\n*>] should be
+            rendered and the result inserted at this point. 
+
+    1. Value-Name-Insertion
+
+    \@_\w+\?? => This indicates the rendering of the node referenced at parent[<\w+>] should be
+            rendered and the result inserted at this point.
+
+    2. Spread-Value-Insertion
+    
+    \@...[.\s] => This indicates that all node.nodes should be rendered and results inserted into
+            the output string with a single character separating the entries. If there are proceeding 
+            Value-Index-Insertion, the spread will start at the index location following the 
+            one specified in Value-Index-Insertion. The spread will be delimited by the character 
+            following [@...]
+
+    3. Non-Value-Insertion 
+    
+    \@\w* => This indicates that a property of node should be inserted into the output
+            string, after being coerced to a string value using the string constructor
+            String()
+
+    3. Conditional-Insertion
+    
+    \@(\w+,\.+) => This indicates the truthiness of a node property determines whether
+            a string expression is   rendered.
+*/
 function buildRendererFromTemplateString<T>(template_pattern: string): RenderAction<T> {
 
     type node = T & { pos: Lexer, nodes: node[], type: number; };
 
-    /* 
-        A template pattern may contain template insertion points marked by a [$]
-        character. For each insertion point there is an action to perform depending
-        on the surrounding syntax:
-        
-        1. Value-Index-Insertion
-        @\n+\?? => This indicates the rendering of the node contained in node.nodes[\n*] should be
-                rendered and the result inserted at this point. 
-
-        1. Value-Name-Insertion
-        @_\w+\?? => This indicates the rendering of the node referenced at node[\w+] should be
-                rendered and the result inserted at this point.
-
-        2. Spread-Value-Insertion
-        @...[.\s] => This indicates that all node.nodes should be rendered and results inserted into
-                the output string with a single character separating the entries. If there are proceeding 
-                Value-Index-Insertion, the spread will start at the index location following the 
-                one specified in Value-Index-Insertion. The spread will be delimited by the character 
-                following [@...]
-
-        3. Non-Value-Insertion 
-        @\w* => This indicates that a property of node should be inserted into the output
-                string, after being coerced to a string value using the string constructor
-                String()
-
-        3. Conditional-Insertion
-        @(\w+,\.+) => This indicates the truthiness of a node property determines whether
-                a string expression is   rendered.
-    */
 
     const
         regexA = /\t|\n|%|0|1|((\@)((\((\w+),([^\)]+)\s*\))|(\_)?(\w+)?(\.\.\.([^]))?(\?)?))|(\\\?|[^01\n?@%])+/g,
