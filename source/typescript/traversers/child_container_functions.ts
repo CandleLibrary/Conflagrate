79d21fd40efd5b1cf07fb770400de55f95a5cb7b
Anthony Weathersby <anthonycweathersby@gmail.com>
2021-01-26 21:16:39 -0700

    Feat: Add ability to traverse nodes that use Maps or Sets for children

--- source/typescript/traversers/child_container_functions.ts
+++ source/typescript/traversers/child_container_functions.ts
@@ -1,16 +1,28 @@
 
 
 export function getChildContainerLength<T, K extends keyof T>(node: T, children_key: K): number {
-    if (node && Array.isArray(node[children_key]))
-        return (<T[]><unknown>node[children_key]).length;
+    if (node) {
+        const obj = node[children_key];
+
+        if (Array.isArray(obj)) {
+            return obj.length;
+        } else if (obj instanceof Map || obj instanceof Set) {
+            return obj.size;
+        }
+    }
 
     return 0;
 };
 
 
 export function getChildContainer<T, K extends keyof T>(node: T, children_key: K): T[] {
-    if (node && Array.isArray(node[children_key])) {
-        return (<T[]><unknown>node[children_key]);
+    if (node) {
+        const obj = node[children_key];
+        if (Array.isArray(obj)) {
+            return (<T[]>obj);
+        } else if (obj instanceof Map || obj instanceof Set) {
+            return (<T[]>(Array.from(obj.values())));
+        }
     }
     return [];
 };
